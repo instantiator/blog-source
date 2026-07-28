319df2091fd6b92f670149cc9ad7232aa8414cbf
Lewis Westbury <lewis@cantab.net>
2026-07-28 12:45:30 +0100

    fix: ensure submodule is initialized before deploy

--- deploy-site.sh
+++ deploy-site.sh
@@ -3,15 +3,21 @@
 # quit script on error
 set -e
 
+export DATE=$(date)
+
+# ensure submodule is initialized
+printf "\033[0;32mInitializing public submodule...\033[0m\n"
+git submodule update --init blog-site/public
+
 # generate the site
 printf "\033[0;32mGenerating site...\033[0m\n"
 pushd blog-site
-hugo 
+hugo
 
 # push content changes to source repo
 printf "\033[0;32mPushing source changes...\033[0m\n"
 git add --all
-msg="content changes ${date}"
+msg="content changes ${DATE}"
 if [ -n "$*" ]; then
 	msg="${msg}: $*"
 fi
@@ -21,8 +27,9 @@ git push
 # push rendered changes to public site
 printf "\033[0;32mDeploying updates...\033[0m\n"
 pushd public
+git checkout main
 git add --all
-msg="rebuilding site ${date}"
+msg="rebuilding site ${DATE}"
 # optionally uses command line arguments as the message
 if [ -n "$*" ]; then
 	msg="${msg}: $*"
@@ -35,7 +42,7 @@ popd # blog-site
 printf "\033[0;32mDone updating public submodule.\033[0m\n"
 
 git add --all
-msg="updated submodules: ${date}"
+msg="updated submodules: ${DATE}"
 git commit -m "$msg"
 git push
 printf "\033[0;32mDone updating repository.\033[0m\n"
