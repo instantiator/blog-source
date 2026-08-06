12319f3a59d19a86c8a5a067417c84f873fb320c
Lewis Westbury <lewis@cantab.net>
2026-08-06 12:53:39 +0100

    content changes Thu Aug  6 12:53:36 BST 2026

--- deploy-site.sh
+++ deploy-site.sh
@@ -17,32 +17,38 @@ hugo
 # push content changes to source repo
 printf "\033[0;32mPushing source changes...\033[0m\n"
 git add --all
-msg="content changes ${DATE}"
-if [ -n "$*" ]; then
-	msg="${msg}: $*"
+if ! git diff --cached --quiet; then
+	msg="content changes ${DATE}"
+	if [ -n "$*" ]; then
+		msg="${msg}: $*"
+	fi
+	git commit -m "$msg"
+	git push
 fi
-git commit -m "$msg"
-git push
 
 # push rendered changes to public site
 printf "\033[0;32mDeploying updates...\033[0m\n"
 pushd public
 git checkout main
 git add --all
-msg="rebuilding site ${DATE}"
-# optionally uses command line arguments as the message
-if [ -n "$*" ]; then
-	msg="${msg}: $*"
+if ! git diff --cached --quiet; then
+	msg="rebuilding site ${DATE}"
+	# optionally uses command line arguments as the message
+	if [ -n "$*" ]; then
+		msg="${msg}: $*"
+	fi
+	git commit -m "$msg"
+	git push
 fi
-git commit -m "$msg"
-git push
 
 popd # public
 popd # blog-site
 printf "\033[0;32mDone updating public submodule.\033[0m\n"
 
 git add --all
-msg="updated submodules: ${DATE}"
-git commit -m "$msg"
-git push
+if ! git diff --cached --quiet; then
+	msg="updated submodules: ${DATE}"
+	git commit -m "$msg"
+	git push
+fi
 printf "\033[0;32mDone updating repository.\033[0m\n"
